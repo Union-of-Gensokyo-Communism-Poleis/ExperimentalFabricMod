6a523be6b9ae1a6833a2800dd219a895114d83e6
HakureiHikaze <wgyaaa123@gmail.com>
2021-11-01 17:33:27 +0800

    Updated language file for exp item

--- src/main/java/beer/ugcp/ExperimentalFabricMod/ExperimentalFabricMod.java
+++ src/main/java/beer/ugcp/ExperimentalFabricMod/ExperimentalFabricMod.java
@@ -4,10 +4,8 @@ import beer.ugcp.ExperimentalFabricMod.Items.EXPItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,18 +19,18 @@ public class ExperimentalFabricMod implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	public static final ItemGroup EFM_Group = FabricItemGroupBuilder.build(
 			new Identifier(MOD_ID, "general"),
-			()->new ItemStack(ExperimentalFabricMod.FABRIC_ITEM)
+			()->new ItemStack(ExperimentalFabricMod.EXP_ITEM)
 	);
 
-	public static final EXPItem FABRIC_ITEM = new EXPItem(
-			new FabricItemSettings().group(ExperimentalFabricMod.EFM_Group)
+	public static final EXPItem EXP_ITEM = new EXPItem(
+			new FabricItemSettings().group(ExperimentalFabricMod.EFM_Group).maxCount(64)
 	);
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID,"fabric_item"), FABRIC_ITEM);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID,"exp_item"), EXP_ITEM);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
