beeaecfee4aa5bbc1ad1d988011e9dabc43c7504
HakureiHikaze <wgyaaa123@gmail.com>
2021-11-04 12:14:27 +0800

    Updated elements enum

---
+++ src/main/java/beer/ugcp/ExperimentalFabricMod/Items/MaterialPileItem/AbMaterialPile.java
@@ -0,0 +1,13 @@
+package beer.ugcp.ExperimentalFabricMod.Items.MaterialPileItem;
+
+import net.minecraft.item.Item;
+
+/**
+ * Abstract material pile class.
+ */
+public abstract class AbMaterialPile extends Item {
+
+    public AbMaterialPile(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/beer/ugcp/ExperimentalFabricMod/Utilities/Enum/ChemicalElements.java
@@ -0,0 +1,14 @@
+package beer.ugcp.ExperimentalFabricMod.Utilities.Enum;
+
+public enum ChemicalElements {
+    H  ,                                                                He ,
+    Li ,Be ,                                        B  ,C  ,N  ,O  ,F  ,Ne ,
+    Na ,Mg ,                                        Al ,Si ,P  ,S  ,Cl ,Ar ,
+    K  ,Ca ,Sc ,Ti ,V  ,Cr ,Mn ,Fe ,Co ,Ni ,Cu ,Zn ,Ga ,Ge ,As ,Sn ,Br ,Kr ,
+    Rb ,Sr ,Y  ,Zr ,Nb ,Mo ,Tc ,Rh ,Rd ,Pd ,Ag ,Cd ,In ,Se ,Sb ,Te ,I  ,Xe ,
+    Cs ,Ba ,La ,Hf ,Ta ,W  ,Re ,Os ,Ir ,Pt ,Au ,Hg ,Tl ,Pb ,Bi ,Po ,At ,Rn ,
+    Fr ,Ra ,Ac ,Rf ,Db ,Sg ,Bh ,Hs ,Mt ,Ds ,Rg ,Cn ,Nh ,Fl ,Mc ,Lv ,Ts ,Og ,
+    Uue,Ubn,
+                Ce ,Pr ,Nd ,Pm ,Sm ,Eu ,Gd ,Td ,Dy ,Ho ,Er ,Tm ,Yb ,Lu,
+                Th ,Pa ,U  ,Np ,Pu ,Am ,Cm ,Bk ,Cf ,Es ,Fm ,Md ,No ,Lr
+}
